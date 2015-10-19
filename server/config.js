cc50588f2c872525127d5a2e27d19226a8a3ac6f
natejlee <nathaniel.jy.lee88@gmail.com>
2015-10-19 11:40:35 -0700

    [init] setup database schema using bookshelf

--- server/config.js
+++ server/config.js
@@ -1,10 +1,65 @@
+var Promise = require('bluebird');
+
 var knex = require('knex')({
-  client: 'postgres',
+  client: process.env.dbClient || 'postgres',
   connection: {
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '',
-    database : 'iplan',
+    host     : process.env.dbHost || '127.0.0.1',
+    user     : process.env.dbUser || 'root',
+    password : process.env.dbPassword || '',
+    database : process.env.dbDatabase || 'iplan',
     charset  : 'utf8'
   }
+});
+
+module.exports = db = require('bookshelf')(knex);
+
+db.plugin('registry');
+
+var buildTable = function(name, callback){
+  return db.knex.schema.hasTable(name)
+  .then(function(exists) {
+    if (exists) {
+      return { name: name, created: false };
+    } else {
+      return db.knex.schema.createTable(name, callback);
+    }
+  })
+  .then(function(response){
+    if (!response.name){
+      qb = response;
+      if (qb) {
+        return { name: name, created: true };
+      } else {
+        return { name: name, created: false };
+      }
+    } else { return response; }
+  });
+};
+
+var events = buildTable('events', function(table){
+  table.increments('id').primary();
+  table.string('name').notNullable();
+  table.date('date');
+
+});
+
+var places = buildTable('places', function(table){
+  table.increments('id').primary();
+  table.string('name').notNullable();
+  table.integer('votes');
+  table.integer('event_id');
+});
+
+var tables = [events, places];
+
+Promise.all(tables)
+.then(function(tables){
+  tables.forEach(function(table){
+    if(table.created){
+      console.log('Bookshelf: created table', table.name);
+    }
+    else {
+      console.log('Bookshelf:', table.name, 'table already exists');
+    };
+  });
 });
