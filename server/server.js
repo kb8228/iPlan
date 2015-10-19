cd58d4d4c7cba1dadce4d1239ce40a6600ddb149
Ksenia Smith <smithkseniav@gmail.com>
2015-10-19 14:58:56 -0700

    [init] setup get and post event routes

--- server/server.js
+++ server/server.js
@@ -13,5 +13,27 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client'));
 
+app.get('/api/events', function(req, res){
+  // here we will eventually fetch events by user
+});
+
+app.get('/api/events/:id', function(req, res, next){
+  var eventId = req.params.id;
+  db.model('Event').fetchById(eventId)
+  .then(function(data){
+    res.json(data.toJSON());
+  });
+});
+
+app.post('/api/events', function(req, res, next){
+  var eventName = req.body.name;
+  db.model('Event').newEvent({name: eventName}).save()
+  .then(function(evt){
+    console.log('event created: ', evt);
+    res.json(evt);
+  })
+});
+
+
 app.listen(process.env.PORT || 3000);
 console.log('Listening...');
