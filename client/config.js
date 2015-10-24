587230ae2fd1dc9157eaedd34a00d9dc70681eb0
natejlee <nathaniel.jy.lee88@gmail.com>
2015-10-24 14:45:00 -0700

    [refactor] modularize angular code structure

---
+++ client/config.js
@@ -0,0 +1,48 @@
+angular.module('iplanApp', ['ngRoute', 'auth0', 'angular-storage', 'angular-jwt'])
+.config(function(authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider){
+  authProvider.init({
+    domain: 'iplan.auth0.com',
+    clientID: 'DIJQASF4ghKIEPsKoDVOLLDwg6gRYbhv'
+  });
+
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    // Return the saved token
+    return store.get('token');
+  }];
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'index.html'
+  })
+  .when('/events/:event_id', {
+    templateUrl: './eventView/eventView.html',
+    controller: 'EventViewController'
+  })
+  .when('auth/facebook', {
+    templateUrl: 'index.html',
+    redirect: false
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
+.run(function($rootScope, auth, store, jwtHelper, $location){
+  auth.hookEvents();
+
+  $rootScope.$on('$locationChangeStart', function() {
+    var token = store.get('token');
+    if (token) {
+      if (!jwtHelper.isTokenExpired(token)) {
+        if (!auth.isAuthenticated) {
+          auth.authenticate(store.get('profile'), token);
+        }
+      } else {
+        // Either show the login page or use the refresh token to get a new idToken
+        $location.path('/');
+      }
+    }
+  });
+});
+
