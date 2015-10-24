734801e1fbee97a76014e0f1c816d4540efe27c8
Jeff <jeff.w.harrison86@gmail.com>
2015-10-23 18:41:43 -0700

    [feat] add connection to heroku postgres database

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 
 var knex = require('knex')({
   client: process.env.dbClient || 'postgres',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host     : process.env.dbHost || '127.0.0.1',
     user     : process.env.dbUser || 'root',
     password : process.env.dbPassword || 'ok',
