8deb64ef74c9f4bbd333dd26214404c1d2988885
natejlee <nathaniel.jy.lee88@gmail.com>
2015-10-24 14:55:03 -0700

    [refactor] delete passport related files

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ var db = require('./config/database');
 var http = require('http');
 var Promise = require('bluebird');
 var bodyParser = require('body-parser');
-var passport = require('passport');
 var session = require('express-session');
-var flash = require('connect-flash');
 var app = express();
 
 require('./models/event');
@@ -13,12 +11,6 @@ require('./models/place');
 require('./models/user');
 require('./collections/events');
 require('./collections/places');
-
-require('./config/passport')(passport);
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-require('./routes')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
