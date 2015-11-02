4c59949cd2fa4d9b11c6534df61990035dca21d8
Ksenia Smith <smithkseniav@gmail.com>
2015-11-02 11:53:52 -0800

    [feat] add welcome intro

--- client/userController.js
+++ client/userController.js
@@ -1,9 +1,22 @@
 (function(){
   angular.module('iplanApp')
-  .controller('UserController', ['auth', UserController]);
+  .controller('UserController', ['auth', UserController])
+  .directive('userViewDir', userViewDir);
 
   function UserController(auth) {
     var self = this;
     self.auth = auth;
+  }
+
+  function userViewDir(){
+    return {
+      restrict: 'E',
+      // scope: {},
+      templateUrl: '/userView.html',
+      replace: true,
+      controller: 'UserController',
+      controllerAs: 'userCtrl',
+      bindToController: true
+    }
   }
 })();
